f2df49790d09776cfd8a64914b0f160bd850baf2
Selmen Rayen <selmanerayene95@gmail.com>
2023-05-13 02:46:36 +0100

    Add loading spinner

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,8 +1,14 @@
-import {  useGetPostsQuery } from "./postsSlice";
+import { useGetPostsQuery } from "./postsSlice";
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import PostsExcerpt from "./PostsExcerpt";
+import { MoonLoader } from "react-spinners";
 
 const PostsList = () => {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+
   const {
     data: posts,
     isLoading,
@@ -13,7 +19,10 @@ const PostsList = () => {
 
   let content;
   if (isLoading) {
-    content = <p>"Loading..."</p>;
+    // content = <p>"Loading..."</p>;
+    content =  
+    <MoonLoader color="#d81cb9" size={250}/>
+
   } else if (isSuccess) {
     content = posts.ids.map((postId) => (
       <PostsExcerpt key={postId} postId={postId} />
